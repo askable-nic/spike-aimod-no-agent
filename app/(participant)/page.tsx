30341944c96c2a6bd8980a47170ab69a0ebee55a
Nic <nic@askable.com>
2024-10-17 14:00:03 +1000

    types fix

--- app/(participant)/page.tsx
+++ app/(participant)/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
   }
   return (
     <RoomWrapper name={roomName} viewer="participant">
-      <ParticipantRoom name={roomName} />
+      <ParticipantRoom />
     </RoomWrapper>
   );
 }
