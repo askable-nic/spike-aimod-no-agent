3a81e97f24d18d847409209884ddbab383169fd4
Nic <nic@askable.com>
2024-10-26 14:56:42 +1000

    non-livekit deepgram recording

--- app/deepgram/Deepgram.tsx
+++ app/deepgram/Deepgram.tsx
@@ -1,118 +1,186 @@
 import { DeepgramClient, LiveTranscriptionEvents } from "@deepgram/sdk";
-import {
-  useLocalParticipant,
-  useRoomContext,
-  ControlBar,
-} from "@livekit/components-react";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
-export const Deepgram = ({ apiKey }: { apiKey: string }) => {
-  const room = useRoomContext();
-  const localParticipant = useLocalParticipant();
-  const deepgram = useRef<DeepgramClient>(new DeepgramClient({ key: apiKey }));
-  const recorder = useRef<MediaRecorder | undefined>();
-  const stt = useRef<string[]>([]);
-  const [lastMessage, setLastMessage] = useState<string | undefined>();
-  const sttPreviewRef = useRef<HTMLPreElement | null>(null);
-  const stopRecordingTimeout = useRef<NodeJS.Timeout | null>(null);
+async function getMicrophone() {
+  const userMedia = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+  });
+  return new MediaRecorder(userMedia);
+}
 
-  const [recorderState, setRecorderState] = useState<
-    "idle" | "starting" | "recording" | "finalising"
+export const Deepgram = ({ dgApiKey }: { dgApiKey: string }) => {
+  const [dgClient, setDgClient] = useState<DeepgramClient | undefined>();
+  const [recordingState, setRecordingState] = useState<
+    "idle" | "initialising" | "recording" | "finalising"
   >("idle");
-  const [sttState, setSttState] = useState<"idle" | "listening" | "finalising">(
-    "idle"
-  );
+  const activeMic = useRef<MediaRecorder | undefined>();
+  const sttInterim = useRef<string>("");
+  const sttTranscript = useRef<string>("");
+  const sttInterimPreview = useRef<HTMLTextAreaElement>(null);
+  const sttTranscriptPreview = useRef<HTMLTextAreaElement>(null);
+  const [lastSubmittedMessage, setLastSubmittedMessage] = useState<string>("");
 
-  const micTrack = localParticipant.microphoneTrack;
-  const micStream = micTrack?.audioTrack?.mediaStream;
+  const sttPreviewInterval = useRef<NodeJS.Timeout | undefined>();
 
   useEffect(() => {
-    if (micStream) {
-      if (localParticipant.isMicrophoneEnabled) {
-        if (micStream && deepgram.current) {
-          const dgConnection = deepgram.current.listen.live({
+    setDgClient(new DeepgramClient({ key: dgApiKey }));
+  }, [dgApiKey]);
+
+  const updatePreviews = useCallback(() => {
+    if (sttInterimPreview.current) {
+      sttInterimPreview.current.value = sttInterim.current ?? "";
+    }
+    if (sttTranscriptPreview.current) {
+      sttTranscriptPreview.current.value = sttTranscript.current;
+    }
+  }, []);
+
+  useEffect(() => {
+    sttPreviewInterval.current = setInterval(updatePreviews, 250);
+    return () => {
+      if (sttPreviewInterval.current) {
+        clearInterval(sttPreviewInterval.current);
+      }
+    };
+  }, []);
+
+  function commitStt() {
+    if (sttInterim.current) {
+      sttTranscript.current += `${sttInterim.current} `;
+      sttInterim.current = "";
+    }
+    return sttTranscript.current;
+  }
+
+  const submitTranscript = useCallback(async (transcriptValue: string) => {
+    if (!transcriptValue) {
+      console.warn("No transcript to submit");
+      return false;
+    }
+    // mock server request
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    setLastSubmittedMessage(transcriptValue);
+  }, []);
+
+  const startRecording = useMemo(
+    () => () => {
+      if (!dgClient) {
+        console.warn("Deepgram client not initialised");
+        return;
+      }
+      setRecordingState("initialising");
+      getMicrophone()
+        .then((mic) => {
+          const dgConnection = dgClient.listen.live({
             model: "nova",
             punctuate: true,
             channels: 1,
-            // endpointing: ENDPOINT_DELAY,
-            interim_results: false,
+            // endpointing: 500,
+            interim_results: true,
           });
-
           dgConnection.on(LiveTranscriptionEvents.Open, () => {
-            setSttState("listening");
             dgConnection.on(LiveTranscriptionEvents.Transcript, (data) => {
-              const transcript = data?.channel?.alternatives[0]?.transcript;
-              if (
-                data?.is_final &&
-                transcript &&
-                typeof transcript === "string"
-              ) {
-                stt.current.push(transcript);
-                if (sttPreviewRef.current) {
-                  sttPreviewRef.current.innerText = stt.current.join("\n");
-                }
-              } else if (transcript) {
-                console.log(data, transcript);
+              sttInterim.current = data?.channel?.alternatives[0]?.transcript;
+              if (data.is_final && sttInterim.current) {
+                commitStt();
+                sttTranscriptPreview.current!.value = sttTranscript.current;
               }
             });
-            setRecorderState("starting");
-            recorder.current = new MediaRecorder(micStream, {});
-            recorder.current.ondataavailable = (event) => {
-              dgConnection.send(event.data);
+            mic.start(500);
+            setRecordingState("recording");
+            mic.ondataavailable = (e) => {
+              const data = e.data;
+              //   console.log("audio data:", data.size);
+              dgConnection.send(data);
             };
-            recorder.current.onstart = () => {
-              setRecorderState("recording");
-            };
-            recorder.current.onstop = () => {
-              setRecorderState("idle");
-            };
-            recorder.current.start(250);
+            activeMic.current = mic;
           });
-        } else {
-          throw new Error("Deepgram client not initialized");
-        }
-      } else {
-        if (recorder.current) {
-          setRecorderState("finalising");
-          setSttState("finalising");
-          stopRecordingTimeout.current = setTimeout(() => {
-            if (recorder.current) {
-              recorder.current.stop();
-              recorder.current = undefined;
-            }
-            setLastMessage(stt.current.join(" "));
-            stt.current = [];
-            if (sttPreviewRef.current) {
-              sttPreviewRef.current.innerText = "";
-            }
-          }, 1000);
-        }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    [dgClient]
+  );
+
+  const stopRecording = useMemo(
+    () => () => {
+      setRecordingState("finalising");
+      if (!activeMic.current) {
+        console.warn("No active microphone");
+        setRecordingState("idle");
+        return;
       }
+      activeMic.current.stop();
+      commitStt();
+      sttTranscriptPreview.current!.value = "";
+      submitTranscript(sttTranscript.current).finally(() => {
+        sttInterim.current = "";
+        sttTranscript.current = "";
+        setRecordingState("idle");
+      });
+    },
+    [submitTranscript]
+  );
+
+  const recordButton = useMemo(() => {
+    switch (recordingState) {
+      case "idle":
+        return (
+          <button className="btn btn-primary" onClick={startRecording}>
+            Start
+          </button>
+        );
+      case "initialising":
+        return (
+          <button className="btn" disabled>
+            Start
+          </button>
+        );
+      case "recording":
+        return (
+          <button className="btn btn-secondary" onClick={stopRecording}>
+            Stop
+          </button>
+        );
+      case "finalising":
+        return (
+          <button className="btn" disabled>
+            Stop
+          </button>
+        );
+      default:
+        return (
+          <button className="btn" disabled>
+            ...
+          </button>
+        );
     }
-    return () => {
-      if (stopRecordingTimeout.current) {
-        clearTimeout(stopRecordingTimeout.current);
-      }
-    };
-  }, [localParticipant.isMicrophoneEnabled, micStream]);
+  }, [recordingState, startRecording, stopRecording]);
 
   return (
     <div className="container my-2 px-2">
-      <h1>Deepgram</h1>
-      <h2>Room: {room.state}</h2>
-      <h3>Recorder state: {recorderState}</h3>
-      <h3>SST state: {sttState}</h3>
-      {/* <button onClick={() => { localParticipant.isMicrophoneEnabled }}> */}
-      <ControlBar />
-      <pre ref={sttPreviewRef} className="text-xs" />
-      <blockquote>
-        {lastMessage ? (
-          <>
-            <strong>Sent:</strong>
-            <p>{lastMessage}</p>
-          </>
-        ) : null}
-      </blockquote>
+      <h1>Deepgram live STT</h1>
+      {recordButton}
+      <h2>Live STT</h2>
+      <h3>Current utterance</h3>
+      <textarea
+        className="textarea text-sm font-mono! leading-tight textarea-bordered w-full"
+        readOnly
+        ref={sttInterimPreview}
+      />
+      <h3>Whole transcript</h3>
+      <textarea
+        className="textarea text-sm font-mono! leading-tight textarea-bordered w-full"
+        readOnly
+        ref={sttTranscriptPreview}
+      />
+      <h2>Submitted message</h2>
+      <textarea
+        className="textarea text-sm font-mono! leading-tight textarea-bordered w-full"
+        readOnly
+        value={lastSubmittedMessage}
+      />
     </div>
   );
 };
--- app/deepgram/page.tsx
+++ app/deepgram/page.tsx
@@ -1,36 +1,14 @@
 "use client";
-import { RoomWrapper } from "@/components/RoomWrapper";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
 import { Deepgram } from "./Deepgram";
 
-const PageRoom = () => {
-  const [dgApiKey, setDgApiKey] = useState<string | undefined>();
-  const [roomName, setRoomName] = useState<string | undefined>();
-
+const ParamsWrapper = () => {
   const params = useSearchParams();
-
-  useEffect(() => {
-    const [_roomName, _dgApiKey] = [
-      params.get("room-name"),
-      params.get("dg-api-key"),
-    ];
-    if (typeof _roomName === "string") {
-      setRoomName(_roomName);
-    }
-    if (typeof _dgApiKey === "string") {
-      setDgApiKey(_dgApiKey);
-    }
-  }, [params]);
-
-  if (dgApiKey && roomName) {
-    return (
-      <RoomWrapper name={roomName} viewer="participant">
-        <Deepgram apiKey={dgApiKey} />
-      </RoomWrapper>
-    );
+  const dgApiKey = params.get("dg-api-key");
+  if (dgApiKey) {
+    return <Deepgram dgApiKey={dgApiKey} />;
   }
-
   return (
     <form
       className="container max-w-xs mx-auto my-4 flex flex-col space-y-2"
@@ -39,16 +17,8 @@ const PageRoom = () => {
       <input
         type="text"
         className="input input-bordered w-full monospace"
-        placeholder="API Key"
+        placeholder="Deepgram API Key"
         name="dg-api-key"
-        value={dgApiKey}
-      />
-      <input
-        type="text"
-        className="input input-bordered w-full monospace"
-        placeholder="Room Name"
-        name="room-name"
-        value={roomName}
       />
       <button type="submit" className="btn">
         Submit
@@ -58,9 +28,5 @@ const PageRoom = () => {
 };
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <PageRoom />
-    </Suspense>
-  );
+  return <Suspense fallback={<div>Loading...</div>}><ParamsWrapper /></Suspense>;
 }
